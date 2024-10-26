11b9ec9502e477ffabdd9941a6d4ea74a777e273
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-10-26 13:39:58 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 set -eux
 
 HOME=$(cygpath -m /home)
-NAME=tor-browser-android-armv7-13.5.2.apk
+NAME=tor-browser-windows-i686-portable-14.0.exe
 cd /home
-wget -q https://dist.torproject.org/torbrowser/13.5.2/tor-browser-android-armv7-13.5.2.apk
+wget -q https://dist.torproject.org/torbrowser/14.0/tor-browser-windows-i686-portable-14.0.exe
 
 if [[ -v GITHUB_WORKFLOW ]]; then
   echo "OUTPUT_BINARY=${HOME}/${NAME}" >> $GITHUB_OUTPUT
